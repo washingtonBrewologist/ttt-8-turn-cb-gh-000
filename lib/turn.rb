c55ec839765816d5167e07b615d12f59f9b8dd62
Nick Williams <nwilliams7539@student.whatcom.edu>
2018-09-14 22:52:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,34 @@
+# Will display our tick tac toe board!
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# Will convert the user's place on the board to the index into
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
+# Validates user entry and checks if position is taken
+def valid_move?(board, index)
+  if (position_taken?(board, index)) == false && index.between?(0,8)
+    return true
+  end
+end    
+def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+def move(board, index, player_char = "X")
+  board[index] = player_char
+  return board
 end
